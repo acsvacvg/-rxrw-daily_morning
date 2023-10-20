36446d7a29c49b0b5fe906c03374f8707f83ea16
acsvacvg <118419131+acsvacvg@users.noreply.github.com>
2023-10-21 00:56:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import requests
 import os
 import random
 import json
+from requests.packages import urllib3
+
 
 today = datetime.now()
 start_date = os.environ['START_DATE']
@@ -54,12 +56,40 @@ def get_words():
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
 
+// 获取星座
+def get_lucky():
+  url = "http://web.juhe.cn:8080/constellation/getAll?consName=金牛座&type=today&key=4a11bbcbf089edaf14c2d9bdb80c2ec4"
+  res = requests.get(url).json()
+  return res['color'],res['summary']
+
+// 新闻
+def get_info():
+  url = "http://v.juhe.cn/toutiao/index?type=yule&key=d268884b9b07c0eb9d6093dc54116018"
+  res = requests.get(url).json()['result']
+  info = res['data'][0]['title']
+  return info
+
+// 获取历史上的今天
+def get_history():
+  url = "https://api.oick.cn/lishi/api.php"
+  res= requests.get(url).json()
+  history = res['result'][0]
+  return history['date'],history['title']
 
 client = WeChatClient(app_id, app_secret)
 today_date = json.dumps(date.today(), cls=ComplexEncoder)
 
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
-data = {"city":{"value":city}, "date":{"value":today_date}, "weather":{"value":wea},"temperature":{"value":str(temperature) + "℃"},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+color,summary = get_lucky()
+date,title = get_history()
+info = get_info()
+
+data = {"city":{"value":city}, "date":{"value":today_date, "color":get_random_color()}, "weather":{"value":wea, "color":get_random_color()},
+        "temperature":{"value":(str(temperature) + "℃"), "color":get_random_color()},
+        "love_days":{"value":get_count(), "color":get_random_color()},"birthday_left":{"value":get_birthday(), "color":get_random_color()},
+        "words":{"value":get_words(), "color":get_random_color()}, "color": {"value": color, "color": get_random_color()}, "date": {"value": date, "color": get_random_color()},
+        "title": {"value": title, "color": get_random_color()}, "summary": {"value": summary, "color": get_random_color()}, 
+        "info": {"value": info, "color": get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
