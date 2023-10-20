e05f22cb31208118e29965840754fca5b7cb4fef
acsvacvg <118419131+acsvacvg@users.noreply.github.com>
2023-10-21 00:34:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -60,6 +60,6 @@ today_date = json.dumps(date.today(), cls=ComplexEncoder)
 
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
-data = {"city":{"value":city}, "date":{"value":today_date}, "weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+data = {"city":{"value":city}, "date":{"value":today_date}, "weather":{"value":wea},"temperature":{"value":str(temperature) + "℃"},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
